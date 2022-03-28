d15b00befc11f947f1cd436db9ff04b9e3906067
Nguyen Tien Dat <datnt@ecotekcorp.com>
2022-03-28 10:39:18 +0700

    [fix] status color on Android

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import HomeStack from './navigator/MovieStack';
+import {Colors} from '../themes';
 
 function AppNavigator() {
   return (
     <>
-      <StatusBar barStyle="light-content" />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={Colors.backgroundScreen}
+      />
       <NavigationContainer>
         <HomeStack />
       </NavigationContainer>
