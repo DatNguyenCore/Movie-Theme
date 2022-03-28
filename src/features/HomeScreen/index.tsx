a891bb116985861f2a3666e2bca6735a95cf75d1
Nguyen Tien Dat <datnt@ecotekcorp.com>
2022-03-28 14:07:33 +0700

    [mod] style FlastList

--- src/features/HomeScreen/index.tsx
+++ src/features/HomeScreen/index.tsx
@@ -2,17 +2,20 @@ import React, {useEffect} from 'react';
 import {FlatList, RefreshControl} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {useSelector, useDispatch} from 'react-redux';
+import {FooterFlatList, NoData} from '../../components';
 import styles from './styles';
 import MovieItem from './components/MovieItem';
 import Header from './components/Header';
 import {NavigationProps} from '../../navigation/configs/NavigationProps';
 import Screen from '../../navigation/configs/Screen';
 import MovieActions from '../../reduxs/reducer/MovieReducer';
+import {RootState} from '../../reduxs';
 
 interface Props extends NavigationProps<Screen.MovieDetail> {}
 
 function HomeScreen({navigation}: Props) {
-  const data = useSelector((state: any) => state.movie.data);
+  const data = useSelector((state: RootState) => state.movie.data);
+  const fetching = useSelector((state: RootState) => state.movie.fetching);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -20,6 +23,16 @@ function HomeScreen({navigation}: Props) {
   }, [dispatch]);
 
   function onRefresh() {}
+
+  function renderFooter() {
+    if (fetching) {
+      return <FooterFlatList />;
+    }
+  }
+
+  function renderEmptyList() {
+    return <NoData />;
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -29,6 +42,8 @@ function HomeScreen({navigation}: Props) {
           <RefreshControl refreshing={false} onRefresh={onRefresh} />
         }
         ListHeaderComponent={<Header />}
+        ListFooterComponent={renderFooter()}
+        ListEmptyComponent={renderEmptyList()}
         keyExtractor={item => item.id.toString()}
         renderItem={({item}) => (
           <MovieItem data={item} navigation={navigation} />
